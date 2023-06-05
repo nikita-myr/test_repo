3f7ed093fda6cefbd42611b5ce35a98f7fa9b0aa
nikita-myr <nik.myr.2000@gmail.com>
2023-06-05 21:39:06 +0400

    added dir test

---
+++ test/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Blank 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SomeClass someClass = new SomeClass();
+            someClass.SomeVoid();
+
+
+
+
+        }
+    }
+
+    public abstract class AbstractClass
+    {
+        public virtual void SomeVoid()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+
+    class SomeClass : IInterface
+    {
+        public void 
+        
+    }
+
+    interface IInterface
+    {
+        void InterfaceVoid();
+
+    }
+    
+    
+    
+}
+
